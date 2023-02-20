422e25062dd597791119268a2d3ed20441629b1f
Saar Turjeman <saarturjeman@gmail.com>
2023-02-20 17:27:41 -0500

    #3

--- assignment3.py
+++ assignment3.py
@@ -6,6 +6,8 @@ import re
 import urllib.request
 
 
+# Saar T. IS211 CUNYSPS SPRING 2023
+
 def download_data(url):
     """
     Reads data from a URL and returns the data as a string
@@ -42,11 +44,8 @@ def main(url):
 
     # Determine the most searched browser in the file
 
-    Firefox = 0
-    Chrome = 0
-    Internet_Explorer = 0
-    Safari = 0
     csv_browsers = {}
+    Firefox = Chrome = MSIE = Safari = 0
     url_data = download_data(url)
     csv_data = csv.reader(io.StringIO(url_data))
     for row in csv_data:
@@ -54,17 +53,37 @@ def main(url):
             Firefox += 1
         elif re.search('Chrome', row[2]):
             Chrome += 1
-        elif re.search('Internet Explorer', row[2]):
-            Internet_Explorer += 1
+        elif re.search('MSIE', row[2]):
+            MSIE += 1
         elif re.search('Safari', row[2]):
             Safari += 1
     csv_browsers["Firefox"] = Firefox
     csv_browsers["Chrome"] = Chrome
-    csv_browsers["Internet Explorer"] = Internet_Explorer
+    csv_browsers["MSIE"] = MSIE
     csv_browsers["Safari"] = Safari
-    popular = max(csv_browsers, key=csv_browsers.get)
-    print(f"The most popular browser is {popular}"
-          f" with {csv_browsers[popular]} hits.")
+    highest_hits = max(csv_browsers, key=csv_browsers.get)
+    print("The browser with the highest hits is", highest_hits,
+          "with", csv_browsers[highest_hits], "hits")
+
+    # Output a list of hours of the day sorted by the total number of hits that occurred in that hour
+    # EXTRA CREDIT
+    hour_list = []
+    url_data = download_data(url)
+    csv_data = csv.reader(io.StringIO(url_data))
+    for row in csv_data:
+        format = '%Y-%m-%d %H:%M:%S'
+        hour_time = datetime.datetime.strptime(row[1], format)
+        hour = hour_time.hour
+        hour_list.append(hour)
+    print("First hour with", hour_list.count(0), "hits")
+    print("Second hour with", hour_list.count(1), "hits")
+    print("Third hour with", hour_list.count(2), "hits")
+    print("Fourth hour with", hour_list.count(3), "hits")
+    print("Fifth hour with", hour_list.count(4), "hits")
+    print("Sixth hour with", hour_list.count(5), "hits")
+
+
+
 
 
 if __name__ == "__main__":
@@ -72,4 +91,5 @@ if __name__ == "__main__":
     parser.add_argument("--url", help="URL to the datafile", type=str, required=True)
     args = parser.parse_args()
     main(args.url)
+
 
